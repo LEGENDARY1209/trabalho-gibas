a49c1dcf70373435ee8022ae70a3e25ddc87282f
Raí <130669909+LEGENDARY1209@users.noreply.github.com>
2024-10-03 10:01:48 -0300

    dwefrtf

---
+++ CRUD_Teste.sql
@@ -0,0 +1,24 @@
+-- Verificar registros da tabela
+SELECT * 
+FROM estudantes;
+
+-- Gravar um registro de teste
+INSERT INTO estudantes(matrícula, nome)
+VALUES('1234', 'João da Silva');
+
+-- Testando Update
+UPDATE estudantes
+SET nome = 'Antônio dos Santos'
+WHERE matricula = '1234';
+
+-- Verificar os registros da tabela
+SELECT *
+FROM estudantes;
+
+-- Excluindo  registro da tabela
+DELETE FROM estudantes
+WHERE matricula = '1234';
+
+-- Verificar registros na tabela
+SELECT *
+FROM estudantes;
